8f612f1494c175bdcdd7781599c0a8f6763495d5
preetikjha <preetikjha@Preetis.attlocal.net>
2015-05-27 00:01:10 -0500

    first time commited

---
+++ MyProj/src/test/java/p1/HomePage.java
@@ -0,0 +1,21 @@
+package p1;
+
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+	WebDriver driver;
+    public   HomePage(WebDriver driver)
+    {
+         this.driver=driver;
+    }
+    public void openUrl()
+    {
+           //   to open the url of flipkart
+    }
+    public void selectProduct()
+    {
+            //   go to electronics
+               //select HP product
+    }
+
+}
